d9acac7af1ac6f854e21a16873210d0b8b324a2a
Jyoshna-2021 <91448597+Jyoshna-2021@users.noreply.github.com>
2021-11-15 09:59:29 +0530

    Add files via upload

---
+++ MINIPROJECT/main.js
@@ -0,0 +1,74 @@
+   
+function setFormMessage(formElement, type, message) {
+    const messageElement = formElement.querySelector(".form__message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form__message--success", "form__message--error");
+    messageElement.classList.add(`form__message--${type}`);
+}
+
+function setInputError(inputElement, message) {
+    inputElement.classList.add("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
+}
+
+function clearInputError(inputElement) {
+    inputElement.classList.remove("form__input--error");
+    inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
+}
+const form=document.getElementById("createAccount");
+const username=document.getElementById("signupUsername");
+const email=document.getElementById("email");
+const password1=document.getElementById("psw");
+const password2=document.getElementById("conpsw");
+function checkInput(){
+    const usernamevalue=username.value.trim();
+    const emailvalue=email.value.trim();
+    const password1value=password1.value.trim();
+    const password2value=password2.value.trim();
+    if(usernamevalue=""){
+        
+    }
+
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form--hidden");
+        createAccountForm.classList.remove("form--hidden");
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form--hidden");
+        createAccountForm.classList.add("form--hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // Perform your AJAX/Fetch login
+
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+    });
+    document.querySelector("submit",e=>{
+e.preventDefault();
+checkInput();
+    });
+
+    document.querySelectorAll(".form__input").forEach(inputElement => {
+        inputElement.addEventListener("blur", e => {
+            if (e.target.id === "signupUsername" && e.target.value.length > 0 && e.target.value.length < 3) {
+                setInputError(inputElement, "Username must be at least 3 characters in length");
+            }
+        });
+
+        inputElement.addEventListener("input", e => {
+            clearInputError(inputElement);
+        });
+    });
+});
